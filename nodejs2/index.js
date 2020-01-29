67b111f815a19b9bc2b9b42f7adaa32cd8f30026
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-01-29 16:03:12 +0700

    nodejs2 update

--- nodejs2/index.js
+++ nodejs2/index.js
@@ -1,17 +1,43 @@
 const express = require("express");
+
+// init app
 const app = express();
 
+// port
 const port = 3000;
 
+// load view engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-
+// home route
 app.get('/', (req, res) => {
-    res.render('index');
+    let users = [
+        {
+            id: 1,
+            name: 'Thien'
+        },
+        {
+            id: 2,
+            name: 'Thinh'
+        },
+        {
+            id: 3,
+            name: 'Thanh'
+        }
+    ];
+    res.render('index', {
+        usersList: 'Users List',
+        users: users
+    });
 });
 
-app.get('/users', (req, res) => {
-    res.render('users');
+// add route
+app.get('/users/add', (req, res) => {
+    res.render('add_user', {
+        usersList: 'add users'
+    });
 });
+
+// start server
 app.listen(port, () => console.log("Listen on port", port));
