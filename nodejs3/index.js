77b0165a77c656983aed3cb56586cb7377d7bd42
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-01-31 15:28:47 +0700

    nodejs3 update

--- nodejs3/index.js
+++ nodejs3/index.js
@@ -1,43 +1,39 @@
 const express = require("express");
 //init app
 const app = express();
+const low = require("lowdb");
+const FileSync = require('lowdb/adapters/FileSync');
+const shortid = require("shortid");
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+db.defaults({users: []})
+    .write();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 // port
 const port = 3000;
-// users list
-let users = [
-    {
-        id: 1,
-        name: 'Thien'
-    },
-    {
-        id: 2,
-        name: 'Thinh'
-    },
-    {
-        id: 3,
-        name: 'Quynh'
-    },
-    {
-        id: 4,
-        name: 'Thanh'
-    }
-];
 
 // set view engine
 app.set('view engine', 'pug');
 app.set('views', './views');
 // home route
 app.get('/', (req, res) => {
-    
     res.render('index', {
-        users: users
+        users: db.get('users').value()
     });
+    
 });
 
 // search  user route
 app.get('/search_user', (req, res) => {
     let name = req.query.name;
-    let matchUsers = users.filter(user => {
+    let matchUsers = db.get('users').value().filter(user => {
         return user.name.indexOf(name) !== -1;
     });
     res.render('search_user', {
@@ -45,6 +41,27 @@ app.get('/search_user', (req, res) => {
     });
 });
 
+// create user get
+app.get('/create_user', (req, res) => {
+    res.render('create_user');
+});
+
+// get id user
+app.get('/:id', (req, res) => {
+    let id = req.params.id;
+    let user = db.get('users').find({ id: id}).value();
+
+    res.render('view', {
+        user: user
+    });
+});
+
+// create user post
+app.post('/create_user', (req, res) => {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/');
+});
 // start server
 app.listen(port, () => {
     console.log("Listen on port ", port);
